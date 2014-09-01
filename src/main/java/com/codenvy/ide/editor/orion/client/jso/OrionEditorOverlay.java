1860e74ee3389eae3faf613e7a07469402f67f68
Mickaël Leduque <mickael.leduque@serli.com>
2014-09-01 16:38:33 +0200

    Fix jsni content in reportStatus methods

--- src/main/java/com/codenvy/ide/editor/orion/client/jso/OrionEditorOverlay.java
+++ src/main/java/com/codenvy/ide/editor/orion/client/jso/OrionEditorOverlay.java
@@ -107,7 +107,7 @@ public class OrionEditorOverlay extends JavaScriptObject {
      * @param message the message
      */
     public final native void reportStatus(String message) /*-{
-        this.type filter text(message);
+        this.reportStatus(message);
     }-*/;
 
     /**
@@ -116,7 +116,7 @@ public class OrionEditorOverlay extends JavaScriptObject {
      * @param type either normal or "progress" or "error";
      */
     public final native void reportStatus(String message, String type) /*-{
-        this.type filter text(message, type);
+        this.reportStatus(message, type);
     }-*/;
 
     /**
@@ -126,6 +126,6 @@ public class OrionEditorOverlay extends JavaScriptObject {
      * @param accessible if true, a screen reader will read this message
      */
     public final native void reportStatus(String message, String type, boolean accessible) /*-{
-        this.type filter text(message, type, accessible);
+        this.reportStatus(message, type, accessible);
     }-*/;
 }
